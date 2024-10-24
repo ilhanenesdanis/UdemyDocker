4c195d9611bbac67ae710587e5a195dd4e82c9b7
ilhan enes daniş <danisenes3@gmail.com>
2024-10-24 12:58:55 +0300

    create mvc app

---
+++ UdemyDockerCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemyDockerCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
